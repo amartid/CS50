7b83df5ffd69d11f431c1fc864cec0dfc156b775
whatfreedom <angelos95gr@gmail.com>
2023-04-10 16:33:07 +0300

    conditionals

---
+++ 1 Conditionals/1_compare/compare2.py
@@ -0,0 +1,11 @@
+# Demonstrates conditionals
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y: #boolean expression
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
---
+++ 1 Conditionals/2_grade/grade3.py
@@ -0,0 +1,13 @@
+score = int(input("Score: "))
+
+if score >= 90:
+    print("Grade: A")
+if score >=80:
+    print("Grade: B")
+if score >= 80:
+    print("Grade: C")
+if score >= 70:
+    print("Grade: D")
+if score >= 60:
+    print("Grade: F")
+# not exclusively exclusive
