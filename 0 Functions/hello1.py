5fdc93d81a54cc730e43aea84bd686d212de558d
whatfreedom <angelos95gr@gmail.com>
2023-04-10 11:08:55 +0300

    first commit

---
+++ 0 Functions/hello1.py
@@ -0,0 +1,22 @@
+
+# print("What's your name?")
+# input()
+# print("hello, world")
+
+# hardcoded:
+# input("What's your name?")
+# print("hello, David")
+
+# Demonstrates a function with a positional argument and a return value
+# storing input to variables
+
+name = input("What's your name? ")
+
+#print("hello, name") #hello, name
+
+print("hello,")
+print(name) #printing the value
+
+print("hello," + name) # Demonstrates concatenation of strings
+
+print("hello,", name) # Demonstrates a function with two positional arguments
