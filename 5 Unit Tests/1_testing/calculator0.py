3c9f2dc5e4b8a4cef160443c17441fc600df732f
whatfreedom <angelos95gr@gmail.com>
2023-04-12 15:45:11 +0300

    unittests

---
+++ 5 Unit Tests/1_testing/calculator0.py
@@ -0,0 +1,14 @@
+
+# Define the main
+def main():
+    x = int(input("What's x?"))
+    print("x squared is", square(x))
+
+# Define a function to calculate the square of a number
+def square(n):
+    return n*n
+
+# This is the standard Python idiom to check if the current file is being run as the main program.
+# If so, it calls the main() function, but if it's being imported as a module, main() won't be executed.
+if __name__== "__main__":
+    main()
